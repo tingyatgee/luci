54a7a74fe728a8536df2ba8fbfc271dc7f52b854
DingYi <tingyatgee@gmail.com>
2025-01-12 17:01:00 +0800

    Update none.js

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/none.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/none.js
@@ -4,7 +4,6 @@
 
 return baseclass.extend({
 	trigger: _('Always off (kernel: none)'),
-	description: _('The LED is always in default state off.'),
 	kernel: true,
 	addFormOptions: function(s) {
 		var o;
