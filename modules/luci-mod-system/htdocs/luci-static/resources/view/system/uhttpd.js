94b4152ab8309c99e8fdc6f49f4c40347bed500d
DingYi <tingyatgee@gmail.com>
2025-01-12 17:03:07 +0800

    Update uhttpd.js

--- modules/luci-mod-system/htdocs/luci-static/resources/view/system/uhttpd.js
+++ modules/luci-mod-system/htdocs/luci-static/resources/view/system/uhttpd.js
@@ -6,12 +6,12 @@ return view.extend({
 	render: function() {
 		var m, s, o;
 
-		m = new form.Map('uhttpd', _('HTTP(S) Access'), _('uHTTPd offers <abbr title="Hypertext Transfer Protocol">HTTP</abbr> or <abbr title="Hypertext Transfer Protocol Secure">HTTPS</abbr> network access.'));
+		m = new form.Map('uhttpd', _('HTTP(S) Access'));
 
 		s = m.section(form.NamedSection, 'main', 'uhttpd', _('Settings'));
 		s.addremove = false;
 
-		o = s.option(form.Flag, 'redirect_https', _('Redirect to HTTPS'), _('Enable automatic redirection of <abbr title="Hypertext Transfer Protocol">HTTP</abbr> requests to <abbr title="Hypertext Transfer Protocol Secure">HTTPS</abbr> port.'));
+		o = s.option(form.Flag, 'redirect_https', _('Redirect to HTTPS'));
 		o.rmempty = false;
 
 		return m.render();
