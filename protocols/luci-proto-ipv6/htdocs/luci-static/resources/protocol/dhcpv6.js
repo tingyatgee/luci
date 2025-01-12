d69145561dd4ca47e24a3722278f87fe1e9a13c4
DingYi <tingyatgee@gmail.com>
2025-01-12 16:47:48 +0800

    Update dhcpv6.js

--- protocols/luci-proto-ipv6/htdocs/luci-static/resources/protocol/dhcpv6.js
+++ protocols/luci-proto-ipv6/htdocs/luci-static/resources/protocol/dhcpv6.js
@@ -19,9 +19,7 @@ return network.registerProtocol('dhcpv6', {
 		o.value('force', _('force', 'DHCPv6 address request mode'));
 		o.value('none', _('disabled', 'DHCPv6 address request mode'));
 		o.default = 'try';
-		o = s.taboption('general', form.Value, 'reqprefix', _('Request IPv6-prefix'),
-			_('Either a prefix length hint (e.g. 56) only, whereby the operator selects the prefix, or specify a prefix also (e.g. %s)')
-			.format('<code>2001:db8::/56</code>'));
+		o = s.taboption('general', form.Value, 'reqprefix', _('Request IPv6-prefix'));
 		o.value('auto', _('Automatic'));
 		o.value('no', _('disabled'));
 		o.value('48');
@@ -31,10 +29,9 @@ return network.registerProtocol('dhcpv6', {
 		o.value('64');
 		o.default = 'auto';
 
-		o = s.taboption('general', form.Flag, 'extendprefix', _('Extend prefix'), _('Extend 3GPP WAN interface /64 prefix via PD to LAN (RFC 7278)'));
+		o = s.taboption('general', form.Flag, 'extendprefix', _('Extend prefix'));
 
-		o = s.taboption('general', form.Flag, 'norelease', _('Do not send a Release when restarting'),
-						_('Enable to minimise the chance of prefix change after a restart'));
+		o = s.taboption('general', form.Flag, 'norelease', _('Do not send a Release when restarting'));
 		o.default = '1';
 		o.rmempty = false;
 
